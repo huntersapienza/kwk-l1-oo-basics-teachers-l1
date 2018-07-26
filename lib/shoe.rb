77591d258e09a74e60940a2235159dfa0daeda6b
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-26 23:02:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,11 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand
+  attr_accessor :brand, :color
   
   def initialize(brand)
     @brand = brand
+    @color = color
+    
   end
   
 end
