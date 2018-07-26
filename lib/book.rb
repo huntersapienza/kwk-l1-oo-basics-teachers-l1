b1cb3ed298193b1114baf2444c471806ec8f4728
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-26 23:01:20 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,13 @@
 # Add you Book class here
 class Book
   
-  attr_accessor :title, :author, :page_count
+  attr_accessor :title, :author, :page_count, :genre
   
   def initialize(title)
     @title = title
     @author = author
     @page_count = page_count
+    @genre = genre
   end
   
 end
